46ff4fc617890da9cb204b913a25e758bc232c42
sreey <sreeyakkanti@gmail.com>
2018-03-24 10:53:29 -0400

    sangularproj

--- SAngularProj/src/app/app.module.ts
+++ SAngularProj/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { routing } from './app.routing';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register/register.component';
+import { FbintegrationComponent } from './login/fbintegration/fbintegration.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    FbintegrationComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
