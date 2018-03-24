ec53b027667310c1957f2890fafc2fd5bda8a859
sreey <sreeyakkanti@gmail.com>
2018-03-24 10:52:28 -0400

    sangularproj

---
+++ SangularProj/src/app/login/fbintegration/fbintegration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fbintegration',
+  templateUrl: './fbintegration.component.html',
+  styleUrls: ['./fbintegration.component.css']
+})
+export class FbintegrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
